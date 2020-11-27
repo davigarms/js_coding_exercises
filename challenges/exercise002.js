8a2aee46145f4bee574e0bd4f71e5d4fd4d1123d
Davi Garms <davigarms@gmail.com>
2020-11-27 16:07:36 +0000

    Implement Exercise002: countSheep

--- challenges/exercise002.js
+++ challenges/exercise002.js
@@ -20,6 +20,7 @@ function getBusNumbers(people) {
 function countSheep(arr) {
   if (arr === undefined) throw new Error("arr is required");
   // Your code here!
+  return arr.filter(x => x=="sheep").length;
 }
 
 function hasMPostCode(person) {
