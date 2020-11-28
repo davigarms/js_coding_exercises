c4c93de6ac229ddaad99bb92a200393c006c39d9
Davi Garms <davigarms@gmail.com>
2020-11-28 22:41:25 +0000

    Implement Exercise005: - findNeedle - getWordFrequencies - fix arrShift

--- challenges/exercise005.js
+++ challenges/exercise005.js
@@ -28,6 +28,7 @@ const sumArrays = arrs => {
 const arrShift = arr => {
   if (arr === undefined) throw new Error("arr is required");
   // Your code here!
+  if (arr.length < 2) return arr;
   var firstElement = arr.shift();
   arr.unshift(arr.pop());
   arr.push(firstElement);
@@ -38,11 +39,27 @@ const findNeedle = (haystack, searchTerm) => {
   if (haystack === undefined) throw new Error("haystack is required");
   if (searchTerm === undefined) throw new Error("searchTerm is required");
   // Your code here!
+  var hasFound = false;
+  Object.values(haystack).some(function(value){
+    if (value.toString().toLowerCase().indexOf(searchTerm.toLowerCase()) > -1) {
+      hasFound = true;
+      return;
+    }
+  });
+  return hasFound;
 };
 
 const getWordFrequencies = str => {
   if (str === undefined) throw new Error("str is required");
   // Your code here!
+  var words = str.toLowerCase().replace(/[!?.,:;]/g, "").split(" ");
+  var uniqueWords = words.filter((item, index) => words.indexOf(item) === index);
+  var countObj = {};
+  uniqueWords.forEach(function(uniqueWord){
+    var eachWord = words.filter((word) => word === uniqueWord);
+    countObj[uniqueWord] = eachWord.length;
+  });
+  return(countObj);
 };
 
 module.exports = {
