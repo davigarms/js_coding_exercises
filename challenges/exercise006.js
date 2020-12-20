fbfa8d3892c78855c88a91239de6352fd0081e75
Davi Garms <davigarms@gmail.com>
2020-12-20 19:22:58 +0000

    Implement sumMultiples in exercise006

--- challenges/exercise006.js
+++ challenges/exercise006.js
@@ -6,6 +6,9 @@
  */
 const sumMultiples = arr => {
   if (arr === undefined) throw new Error("arr is required");
+  const multipleArr = arr.filter(element => element % 3 === 0 || element % 5 === 0);
+  const total = multipleArr.reduce((a, b) => a + b);
+  return total;
 };
 
 /**
--- test/exercise006.test.js
+++ test/exercise006.test.js
@@ -7,7 +7,12 @@ const {
     areWeCovered
 } = require("../challenges/exercise006");
 
-describe("", () => {
-    test("", () => { 
+
+describe("sumMultiples", () => {
+    test("it returns the sum of any numbers which are a  multiple of 3 or 5",() => { 
+        expect(sumMultiples([3,5])).toBe(8);
+        expect(sumMultiples([1,3,5])).toBe(8);
+        expect(sumMultiples([2,3,5,10])).toBe(18);
+        expect(sumMultiples([1,4,5,9,10])).toBe(24);
     })
 });
