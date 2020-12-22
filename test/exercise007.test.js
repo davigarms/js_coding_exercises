67a2ae81df7d602b88ca1f36759ad0bf09d98a51
Davi Garms <davigarms@gmail.com>
2020-12-22 16:25:51 +0000

    Group similar tests in exercise007/sumDigits

--- test/exercise007.test.js
+++ test/exercise007.test.js
@@ -9,11 +9,8 @@ const {
 describe("sumDigits", () => {
     test("it takes a number and returns the sum of all its digits",() => { 
         expect(sumDigits(123)).toBe(6);
-    })
-    test("it takes a number and returns the sum of all its digits",() => { 
         expect(sumDigits(1234)).toBe(10);
-    })
-    test("it takes a number and returns the sum of all its digits",() => { 
+        expect(sumDigits(459)).toBe(18);
         expect(sumDigits(45329)).toBe(23);
     })
 })
